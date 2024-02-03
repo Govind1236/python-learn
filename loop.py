c91bd1e9038b794e637d10b7c58c212484fcc710
Govind <govincbasnet02@gmail.com>
2024-02-03 20:34:44 +0545

    feat: add for each loop

--- loop.py
+++ loop.py
@@ -1,10 +1,15 @@
-x = 0
-while x < 5:
-    if(x == 3):
-        x += 1
-        continue
-    if (x == 5):
-        break
-    print("X:", x)
-    x +=1
+# x = 0
+# while x < 5:
+#     if(x == 3):
+#         x += 1
+#         continue
+#     if (x == 5):
+#         break
+#     print("X:", x)
+#     x +=1
     
+fruits = ["apple", "guava", "kiwi"]
+for fruit in fruits:
+    if(fruit == "guava"):
+        fruit = "Banana"
+    print("Furits:", fruit)
