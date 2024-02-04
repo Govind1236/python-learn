e375bed01000e899e50b1fe15912d9181beaee80
Govind <govincbasnet02@gmail.com>
2024-02-04 14:53:15 +0545

    feat: grade check

--- task.py
+++ task.py
@@ -6,4 +6,17 @@
 #     C: 70-79
 #     D: 60-69
 #     F: 0-59
- score = int(input("Enter Your Obtain Marks to check Grade: "))
+score = int(input("Enter Your Marks You Obtained: "))
+if score >= 90:
+    print("Your Grade of ", score, "is A")
+elif score >= 80:
+    print("Your Grade of ", score, "is B")
+elif score >= 70:\
+    print("Your Grade of ", score, "is C")
+elif score >= 60:
+    print("Your Grade of ", score, "is D")
+else:
+    print("Your Grade of ", score, "is F")
+    
+    
+          
