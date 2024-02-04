83d2d21c2ba85463d20e884a071dc71f1cb1c110
Govind <govincbasnet02@gmail.com>
2024-02-04 14:34:15 +0545

    Fix: add time formate

---
+++ task.py
@@ -0,0 +1,9 @@
+# Write a Python program that takes a student's
+# score as an input and checks and prints their corresponding grade. Use the following grading scale:
+
+#     A: 90-100
+#     B: 80-89
+#     C: 70-79
+#     D: 60-69
+#     F: 0-59
+ score = int(input("Enter Your Obtain Marks to check Grade: "))
--- time_1.py
+++ time_1.py
@@ -1,9 +1,9 @@
 import time
 name = input("Enter Your Name: ")
-times = time.strftime("%H:%M")
-hour = int(time.strftime("%M"))
+times = time.strftime("%I:%H:%M")
+hour = int(time.strftime("%I"))
 if hour < 12:
-    print("Good morning!! " +  name.upper)
+    print("Good morning!! " +  name.upper(), times)
 else:
-    print("Good afternoon!! " +  name)
+    print("Good afternoon!! " +  name.lower(), times)
 
