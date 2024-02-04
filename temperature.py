342a51b7b6548fc8855491e2207b1e121f3e730e
Govind <govincbasnet02@gmail.com>
2024-02-04 15:12:18 +0545

    add fahrenheit conversion

---
+++ temperature.py
@@ -0,0 +1,10 @@
+
+temperature_tmp = float(input("Enter a temperature in Fahrenheit: "))
+if temperature_tmp < 32:
+    print ("Below Freezing")
+elif temperature_tmp > 32 or  temperature_tmp <= 212:
+    print ("Freezing to boiling")
+else:
+    print ("Boiling and Above")
+
+    
