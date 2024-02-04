64c496d2723ee86c25cd9d12cbcccf8799d792bd
Govind <govincbasnet02@gmail.com>
2024-02-04 13:20:31 +0545

    feat: add user_input

--- time_1.py
+++ time_1.py
@@ -1,11 +1,8 @@
 import time
 name = input("Enter Your Name: ")
-timestamp = time.strftime("%H:%M")
-times = int(time.strftime('%H'))
-print(name , timestamp)
-if(times < 12 ):
-    greet = name + "Good morning!!" + str(times)
-    print(greet)
+times = time.strftime("%H:%M")
+hour = int(time.strftime("%M"))
+if hour < 12:
+    print("Good morning!! " +  name.upper)
 else:
-    greet = name + "Good Afternoon!!" + str(times)
-    print(greet)
+    print("Good afternoon!! " +  name)
