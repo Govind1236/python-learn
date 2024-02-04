50793befe9b4a327771199215f4aca0117e69bf4
Govind <govincbasnet02@gmail.com>
2024-02-04 11:41:55 +0545

    fix: add hour

--- time_1.py
+++ time_1.py
@@ -1,3 +1,11 @@
 import time
-timestamp = time.strftime('%I : %M')
-print(timestamp)
+name = input("Enter Your Name: ")
+timestamp = time.strftime("%H:%M")
+times = int(time.strftime('%H'))
+print(name , timestamp)
+if(times < 12 ):
+    greet = name + "Good morning!!" + str(times)
+    print(greet)
+else:
+    greet = name + "Good Afternoon!!" + str(times)
+    print(greet)
