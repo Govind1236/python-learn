8b2dae382ca777341622419288145c0be9712107
Govind <govincbasnet02@gmail.com>
2024-02-05 15:36:15 +0545

    Fix: add validation for user

--- exercise1.py
+++ exercise1.py
@@ -1,9 +1,7 @@
 import time
 user_name = input("Enter Your Name: ")
-timestamp = time.strftime("%H:%M:S")
+# timestamp = time.strftime("%H:%M:S")
 hour = int(time.strftime("%M"))
-print(timestamp)
-print(user_name)
 if hour < 12:
     print("Good moring!!", user_name)
 else:
---
+++ iteration-over-string.py
@@ -0,0 +1,3 @@
+user_name =  input("Enter user Name: ")
+for i in user_name:
+ print(i.upper(), end= ".") 
